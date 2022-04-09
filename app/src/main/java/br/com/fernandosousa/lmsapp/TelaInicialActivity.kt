c7f10f9747c464a61149b54cd8642c3751835ca9
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-09 12:44:44 -0300

    atualização

--- app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
@@ -4,10 +4,16 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
+import kotlinx.android.synthetic.main.toolbar.*
+
 
 class TelaInicialActivity : DebugActivity() {
 
@@ -31,13 +37,64 @@ class TelaInicialActivity : DebugActivity() {
         mensagemInicial.text = "Bem vindo $nome"
 
         botaoSair.setOnClickListener {cliqueSair()}
+
+        // colocar toolbar
+        setSupportActionBar(toolbar)
+
+
+        // alterar título da ActionBar
+        supportActionBar?.title = "Disciplinas"
+
+        // up navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     fun cliqueSair() {
         val returnIntent = Intent();
-        returnIntent.putExtra("result","Saída do LMSApp");
+        returnIntent.putExtra("result","Saída do BrewerApp");
         setResult(Activity.RESULT_OK,returnIntent);
         finish();
     }
+
+    // método sobrescrito para inflar o menu na Actionbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // infla o menu com os botões da ActionBar
+        menuInflater.inflate(R.menu.menu_main, menu)
+        // vincular evento de buscar
+        (menu?.findItem(R.id.action_buscar)?.actionView as SearchView?)?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // ação enquanto está digitando
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // ação  quando terminou de buscar e enviou
+                return false
+            }
+
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // id do item clicado
+        val id = item?.itemId
+        // verificar qual item foi clicado e mostrar a mensagem Toast na tela
+        // a comparação é feita com o recurso de id definido no xml
+        if  (id == R.id.action_buscar) {
+            Toast.makeText(context, "Botão de buscar", Toast.LENGTH_LONG).show()
+        } else if (id == R.id.action_atualizar) {
+            Toast.makeText(context, "Botão de atualizar", Toast.LENGTH_LONG).show()
+        } else if (id == R.id.action_config) {
+            Toast.makeText(context, "Botão de configuracoes", Toast.LENGTH_LONG).show()
+        }
+        // botão up navigation
+        else if (id == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 }
