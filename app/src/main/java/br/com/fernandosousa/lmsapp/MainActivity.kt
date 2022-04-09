64133314467f95ce6dff9d65fe0eea100709e29d
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-09 12:28:06 -0300

    atualização

--- app/src/main/java/br/com/fernandosousa/lmsapp/MainActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/MainActivity.kt
@@ -2,10 +2,36 @@ package br.com.fernandosousa.lmsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+        // encontra objeto pelo id
+        campo_imagem.setImageResource(R.drawable.imagem_login)
+
+
+        texto_login.text = getString(R.string.mensagem_login)
+
+        // evento no botao de login forma 1
+//        botao_login.setOnClickListener {
+//            val valorUsuario = campo_usuario.text.toString()
+//            val valorSenha = campo_senha.text.toString()
+//            Toast.makeText(this, "$valorUsuario : $valorSenha", Toast.LENGTH_LONG).show()
+//        }
+
+        // segunda forma: delegar para método
+        botao_login.setOnClickListener {onClickLogin() }
+
+    }
+
+    fun onClickLogin(){
+        val valorUsuario = campo_usuario.text.toString()
+        val valorSenha = campo_senha.text.toString()
+        Toast.makeText(this, "$valorUsuario : $valorSenha", Toast.LENGTH_LONG).show()
     }
 }
